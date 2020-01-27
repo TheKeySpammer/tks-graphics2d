f44ddc0143d070e8cf39c63a559426fb88c778cc
theKeySpammer <gt_ams@yahoo.in>
2020-01-27 19:27:15 +0530

    first commit

---
+++ src/main/java/com/tks/graphics/App.java
@@ -0,0 +1,17 @@
+package com.tks.graphics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public String sayHello() {
+        return "Hello";
+    }
+}
